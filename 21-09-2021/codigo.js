e15bb1499ff877333596a0be360fa3d1e016948d
Enrique190999 <71629798+Enrique190999@users.noreply.github.com>
2021-09-21 14:08:00 +0200

    actividad 1

---
+++ 21-09-2021/codigo.js
@@ -0,0 +1,9 @@
+function saludar(){
+    window.alert("Has pulsado un botón");
+    console.log("boton ok")
+}
+
+function funcion(){
+    window.alert("1,2,3,4,5")
+    console.log("numeros ok")
+}
